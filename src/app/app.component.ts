60d6f67c3e120a2624fac83f28891285bb617f8c
luiz gomes <luizgomes77777@gmail.com>
2022-09-07 20:15:59 -0300

    finalizando projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'http-retry';
+
+  constructor(
+    private appService: AppService
+  ) {}
+
+  ngOnInit(): void {
+    this.appService.findAll().subscribe(e => {
+      console.log(e);
+    })
+  }
+
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppComponent } from './app.component';
+
+const toastrConfig = {
+  maxOpened: 1,
+  timeOut: 10000,
+  enableHtml: true,
+  autoDismiss: true,
+  preventDupicates: true,
+  positionClass: 'toast-top-right'
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ToastrModule.forRoot(toastrConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
